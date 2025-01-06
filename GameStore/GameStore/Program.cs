7bc1fc1ba47a8f319b43e80e3540c997d67110be
celilensar <113338225+celilensar@users.noreply.github.com>
2025-01-06 15:26:22 +0300

    Added Entity Framework Core DB context

--- GameStore/GameStore/Program.cs
+++ GameStore/GameStore/Program.cs
@@ -1,9 +1,12 @@
+using GameStore.Data;
 using GameStore.Dtos;
 using GameStore.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+var connString = builder.Configuration.GetConnectionString("GameStore");
+builder.Services.AddSqlite<GameStoreContext>(connString);
 
 app.MapGamesEndPoints();
 
