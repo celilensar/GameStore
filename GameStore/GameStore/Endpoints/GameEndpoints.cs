8bb87a17a39a4e134c9b60e70455942acc9a4c43
celilensar <113338225+celilensar@users.noreply.github.com>
2025-01-06 13:54:17 +0300

    Endpoints and clear code structure.

--- GameStore/GameStore/Endpoints/GameEndpoints.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace GameStore.Endpoints;
-
-public class GamesEndpoints
-{
-
-}
--- GameStore/GameStore/Endpoints/GamesEndpoints.cs
+++ GameStore/GameStore/Endpoints/GamesEndpoints.cs
@@ -1,8 +1,113 @@
 using System;
+using GameStore.Dtos;
 
 namespace GameStore.Endpoints;
 
-public class GameEndpoints
+public static class GameEndpoints
 {
+    const string GetGameEndpointName="GetGame";
+    private readonly static List<GameDto> games = [
+    new (
+    1,
+    "The Legend of Zelda: Breath of the Wild",
+    "Adventure",
+    59.99M,
+    new DateOnly(2017, 3, 3)
+),
+new (
+    2,
+    "Cyberpunk 2077",
+    "Action RPG",
+    49.99M,
+    new DateOnly(2020, 12, 10)
+),
+new (
+    3,
+    "Minecraft",
+    "Sandbox",
+    26.95M,
+    new DateOnly(2011, 11, 18)
+),
+new (
+    4,
+    "Elden Ring",
+    "Action RPG",
+    59.99M,
+    new DateOnly(2022, 2, 25)
+),
+new (
+    5,
+    "Red Dead Redemption 2",
+    "Action-Adventure",
+    59.99M,
+    new DateOnly(2018, 10, 26)
+)
 
+];
+
+
+
+
+public static RouteGroupBuilder MapGamesEndPoints(this WebApplication app )
+{
+    var group = app.MapGroup("games");
+//GET "games"
+group.MapGet("/", () => games);
+
+//GET /games/4
+group.MapGet("/{id}", (int id) =>
+{
+   GameDto? game= games.Find(game => game.Id == id);
+    
+    return game is null ? Results.NotFound() : Results.Ok(game);
 }
+).WithName(GetGameEndpointName);
+
+//POST /games
+group.MapPost("/", (CreateGameDto newGame) => 
+{
+    GameDto game= new(
+        games.Count +1,
+        newGame.Name,
+        newGame.Genre,
+        newGame.Price,
+        newGame.ReleaseDate
+    );
+
+    games.Add(game);
+    return Results.CreatedAtRoute(GetGameEndpointName, new {id = game.Id}, game);
+});
+
+//PUT /games
+
+group.MapPut("/{id}",(int id, UpdateGameDto updatedGame)=>
+
+{
+    var index= games.FindIndex(game=> game.Id == id);
+    
+    if (index == -1){
+        Results.NotFound();
+    }
+
+    games[index] = new GameDto(
+    id,
+    updatedGame.Name,
+    updatedGame.Genre,
+    updatedGame.Price,
+    updatedGame.ReleaseDate);
+    
+    return Results.NoContent();
+});
+
+group.MapDelete("/{id}", (int id)=>{
+    games.RemoveAll(game=>game.Id == id);
+    return Results.NoContent();
+});
+
+
+return group;
+}
+}
+
+
+
